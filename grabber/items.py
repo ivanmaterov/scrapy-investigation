6ccea67f9f607fe8501e68b7c53853f48d789dee
Ivan Materov <ivan.materov@saritasa.com>
2022-04-07 11:39:52 +0700

    Added DetailItem Added type hints
    Task: SELFEDU-18

--- grabber/items.py
+++ grabber/items.py
@@ -3,10 +3,54 @@
 # See documentation in:
 # https://docs.scrapy.org/en/latest/topics/items.html
 
-import scrapy
+from dataclasses import dataclass, field
+from typing import Optional
+from itemloaders.processors import TakeFirst, MapCompose
 
 
-class GrabberItem(scrapy.Item):
-    # define the fields for your item here like:
-    # name = scrapy.Field()
-    pass
+def cut_last_two_symbols(value: str) -> str:
+    """Cut two symbols.
+
+    Data from mirasmart contains two strage symbols in the end of string
+
+    Args:
+        value(str): string with two extra symbols
+
+    Returns:
+        string without two last symbols
+
+    """
+    return value[:-2]
+
+
+@dataclass
+class DetailItem:
+    """Item for storing data."""
+    session_name: Optional[str] = field(
+        default=None,
+        metadata=dict(
+            input_processor=MapCompose(cut_last_two_symbols),
+            output_processor=TakeFirst(),
+        )
+    )
+    topic: Optional[str] = field(
+        default=None,
+        metadata=dict(
+            input_processor=MapCompose(cut_last_two_symbols),
+            output_processor=TakeFirst(),
+        )
+    )
+    program_number: Optional[str] = field(
+        default=None,
+        metadata=dict(
+            input_processor=MapCompose(cut_last_two_symbols),
+            output_processor=TakeFirst(),
+        )
+    )
+    abstract: Optional[str] = field(
+        default=None,
+        metadata=dict(
+            input_processor=MapCompose(cut_last_two_symbols),
+            output_processor=TakeFirst(),
+        )
+    )
--- grabber/spiders/mirasmart_grabber.py
+++ grabber/spiders/mirasmart_grabber.py
@@ -1,5 +1,9 @@
 import math
 import scrapy
+from grabber.items import DetailItem
+from scrapy.loader import ItemLoader
+from scrapy.http.response.html import HtmlResponse
+from scrapy.http.request import Request
 
 
 DETAILS_PER_PAGE = 5
@@ -10,7 +14,7 @@ class QuotesSpider(scrapy.Spider):
 
     start_urls = ("https://index.mirasmart.com/AAN2020/SearchResults.php", )
 
-    def parse(self, response):
+    def parse(self, response: HtmlResponse) -> Request:
         """Entry parse function."""
         details_amount = int(response.xpath("//div[@class='full pagination offset']/p//text()").get().split(" ")[-1])
         page_amount = math.ceil(details_amount / DETAILS_PER_PAGE)
@@ -20,24 +24,40 @@ class QuotesSpider(scrapy.Spider):
                 callback=self.parse_page,
             )
 
-    def parse_page(self, response):
+    def parse_page(self, response: HtmlResponse) -> Request:
         """Parse certain page."""
         search_results = response.xpath("//div[@class='full search-result']")
         abstract_link = search_results.xpath("ul[@class='downloads']/li/a/@href").getall()
         for num, details in enumerate(map(lambda elem: elem.xpath("div[@class='detail']"), search_results)):
             for detail in details:
-                event_details = {
-                    "session_name": detail.xpath("child::node()[3]/span//text()").get(),
-                    "topic": detail.xpath("child::node()[5]/span/a//text()").get(),
-                    "program_number": detail.xpath("child::node()[6]/span//text()").get(),
-                }
+                detail_item = ItemLoader(item=DetailItem(), selector=detail)
+                detail_item.add_xpath(
+                    "session_name",
+                    "child::node()[3]/span//text()",
+                )
+                detail_item.add_xpath(
+                    "topic",
+                    "child::node()[5]/span/a//text()",
+                )
+                detail_item.add_xpath(
+                    "program_number",
+                    "child::node()[6]/span//text()",
+                )
+
                 yield response.follow(
                     url=abstract_link[num],
                     callback=self.parse_abstract_page,
-                    cb_kwargs={"event_details": event_details},
+                    cb_kwargs={"detail_item": detail_item},
                 )
 
-    def parse_abstract_page(self, response, event_details):
+    def parse_abstract_page(
+        self,
+        response: HtmlResponse,
+        detail_item: ItemLoader
+    ) -> DetailItem:
         """Parse abstract page."""
-        event_details["abstract"] = str(response.body)
-        yield event_details
+        detail_item.add_value(
+            "abstract",
+            str(response.body),
+        )
+        yield detail_item.load_item()
